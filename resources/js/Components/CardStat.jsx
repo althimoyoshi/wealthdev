863191ce9d427db3f82f08b776ecf882b10991b6
Timoyoshi <althimoyoshi@gmail.com>
2025-10-17 18:22:27 +0700

    feat(components): enhance CardStat with icon mapping and responsive layout

---
+++ resources/js/Components/CardStat.jsx
@@ -0,0 +1,23 @@
+import { Card, CardHeader, CardTitle, CardContent } from "@/Components/ui/card";
+import { cn } from "@/lib/utils";
+
+export default function CardStat({ data, children }) {
+  const {
+    title,
+    background,
+    className = "",
+    icon: Icon,
+    iconClassName = "",
+  } = data;
+
+  return (
+    <Card className={cn(background, className)}>
+      <CardHeader className="flex flex-row items-start justify-between space-y-0 pb-2">
+        <CardTitle className="font-medium text-wrap text-ms">{title}</CardTitle>
+        {Icon && <Icon className={cn("size-8", iconClassName)} />}
+      </CardHeader>
+
+      <CardContent>{children}</CardContent>
+    </Card>
+  );
+}
