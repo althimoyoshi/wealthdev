9d50e45608312945f4491cf227d5dc3f039cbd31
Timoyoshi <althimoyoshi@gmail.com>
2025-12-05 14:04:19 +0700

    style(ui): refine ApplicationLogo gradient and layout

--- resources/js/Components/ApplicationLogo.jsx
+++ resources/js/Components/ApplicationLogo.jsx
@@ -1,9 +1,13 @@
-import { Link } from '@inertiajs/react';
-export default function ApplicationLogo({ url = '#' }) {
+import { Link } from "@inertiajs/react";
+export default function ApplicationLogo({url = '#'}) {
     return (
-        <Link href={url} className="mt-6 flex items-center gap-x-2">
-            <div className="rounded-xl bg-gradient-to-br from-[#33ff05]/70 via-[#2ee605]/60 to-[#29cc04]/50 p-2">
-                <IconWealth className="size-6 text-white" />
+        <Link
+            href={url}
+            className="mt-6 flex items-center gap-x-2"
+        >
+
+            <div className="p-2 rounded-xl bg-gradient-to-br from-[#33ff05]/70 via-[#2ee605]/60 to-[#29cc04]/50">
+                <IconWealth className="text-whnpmite size-6"/>
             </div>
 
             <span className="text-xl font-semibold leading-relaxed tracking-wide dark:text-lime-500">
