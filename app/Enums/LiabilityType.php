ef244c49a2ba3792f4fa7051a03fa3e133d85620
Timoyoshi <althimoyoshi@gmail.com>
2025-12-05 13:58:12 +0700

    fix(enum): enhance LiabilityType::options to support exclude list

--- app/Enums/LiabilityType.php
+++ app/Enums/LiabilityType.php
@@ -1,5 +1,6 @@
 <?php
 
+
 namespace App\Enums;
 
 enum LiabilityType: string
@@ -9,9 +10,10 @@ enum LiabilityType: string
     case MIDTERMDEBT = 'Hutang Jangka Menengah';
     case LONGTERMDEBT = 'Hutang Jangka Panjang';
 
-    public static function options(): array
+    public static function options(array $exclude = []): array
     {
         return collect(self::cases())
+            ->filter(fn ($item) => ! in_array($item->name, $exclude))
             ->map(fn ($item) => [
                 'value' => $item->name,
                 'label' => $item->value,
